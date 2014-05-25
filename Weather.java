4bbdf53106fee31b1c39251afdf54052b39b711a
delvedor <tomas@delved.org>
2014-05-25 15:10:23 +0200

    First Code Commit.

---
+++ Weather.java
@@ -0,0 +1,100 @@
+class Weather {
+  
+  private XML root;
+  private XML channel;
+
+  public Weather() {
+    root = loadXML("http://weather.yahooapis.com/forecastrss?w=725383&u=c");
+    channel = root.getChild("channel");
+  }
+  
+  /*
+   * Current Day
+   */
+  public String getCityName() {
+    return channel.getChild("yweather:location").getString("city");
+  }
+  
+  public String getCountryName() {
+    return channel.getChild("yweather:location").getString("country");
+  }
+  
+  public String getWeatherCondition() {
+    return channel.getChild("item").getChild("yweather:condition").getString("text");
+  }
+  
+  public String getSunrise() {
+    return channel.getChild("yweather:astronomy").getString("sunrise");
+  }
+  
+  public String getSunset() {
+    return channel.getChild("yweather:astronomy").getString("sunset");
+  }
+  
+  public float getPressure() {
+    return channel.getChild("yweather:atmosphere").getFloat("pressure");
+  }
+  
+  public int getHumidity() {
+    return channel.getChild("yweather:atmosphere").getInt("humidity");
+  }
+  
+  public int getTemperature() {
+    return channel.getChild("item").getChild("yweather:condition").getInt("temp");
+  }
+  
+  public int getWeatherConditionCode() {
+    return channel.getChild("item").getChild("yweather:condition").getInt("code");
+  }
+  
+  /*
+   * Tomorrow
+   */
+  public int getTemperatureLowTomorrow() {
+    return channel.getChild("item").getChild(17).getInt("low");
+  }
+  
+  public int getTemperatureHighTomorrow() {
+    return channel.getChild("item").getChild(17).getInt("high");
+  }
+  
+  
+  public String getWeatherConditionTomorrow() {
+    return channel.getChild("item").getChild(17).getString("text");
+  }
+  
+  public int getWeatherConditionCodeTomorrow(){
+    return channel.getChild("item").getChild(17).getInt("code");
+  }
+  
+  
+  public String getWeekdayTomorrow() {
+    return channel.getChild("item").getChild(17).getString("day");
+  }
+  
+  /*
+   * Day After Tomorrow
+   */
+  public int getTemperatureLowDayAfterTomorrow() {
+    return channel.getChild("item").getChild(19).getInt("low");
+  }
+  
+  public int getTemperatureHighDayAfterTomorrow() {
+    return channel.getChild("item").getChild(19).getInt("high");
+  }
+  
+  
+  public String getWeatherConditionDayAfterTomorrow() {
+    return channel.getChild("item").getChild(19).getString("text");
+  }
+  
+  public int getWeatherConditionCodeDayAfterTomorrow(){
+    return channel.getChild("item").getChild(19).getInt("code");
+  }
+  
+  
+  public String getWeekdayDayAfterTomorrow() {
+    return channel.getChild("item").getChild(19).getString("day");
+  }
+
+}
